e6bb80e2dc48e68c6b7cd4da56d5729fc7c26484
root <root@LAPTOP>
2025-10-02 11:41:41 -0600

    Agregar archivos de proyecto.

---
+++ Models/Persona.vb
@@ -0,0 +1,50 @@
+﻿Public Class Persona
+
+    Private _nombre As String
+    Private _apellido As String
+    Private edad As Integer
+
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Public Property Apellido As String
+        Get
+            Return _apellido
+        End Get
+        Set(value As String)
+            _apellido = value
+        End Set
+    End Property
+
+    Public Property Edad1 As Integer
+        Get
+            Return edad
+        End Get
+        Set(value As Integer)
+            edad = value
+        End Set
+    End Property
+
+
+
+    Public Sub New(nombre As String, apellido As String, edad1 As Integer)
+        Me.Nombre = nombre
+        Me.Apellido = apellido
+        Me.Edad1 = edad1
+    End Sub
+
+    Public Sub New(v As Integer)
+        ' Constructor por defecto
+        Me.Nombre = "No hay nombre"
+
+    End Sub
+
+    Public Sub New()
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("Persona")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Persona")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("ca698077-d1fd-49d2-9927-d3ff6b578e24")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
